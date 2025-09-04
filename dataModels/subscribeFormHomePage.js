9c2885e7a36c7df9e5202f60f61274724aa936a6
Tarun Ahuja <tarunahuja199@gmail.com>
2025-09-04 10:55:23 +0530

    Remove hardcoded MongoDB URI and use .env

--- dataModels/subscribeFormHomePage.js
+++ dataModels/subscribeFormHomePage.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const mongoose=require('mongoose')
-MONGO_URI='mongodb+srv://tarunahuja199_db_user:2wQlkWn4CCt1Tx2@cluster0.mongodb.net/yourDB'
 mongoose.connect(process.env.MONGO_URI)
 const subscribeFormSchema=new mongoose.Schema({
     First:String,
